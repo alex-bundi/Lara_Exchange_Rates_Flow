ca3953f5abb4e3149a703b4223b3dc6e2a752d6a
Alex Bundi <bundialex45@gmail.com>
2024-01-31 17:53:59 +0300

    User Processes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
     ],
 
     theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px'
+          },
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
