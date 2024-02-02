685691d8e494c799e6142d8508a11c892ea06e59
Alex Bundi <bundialex45@gmail.com>
2024-02-02 10:57:12 +0300

    User Processes

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,27 @@ module.exports = {
             lg: '1024px',
             xl: '1280px'
           },
+        fontSize: {
+            'exs':'0.40rem',
+            'xxs': '0.60rem',
+            'sm': '0.8rem',
+            'base': '1rem',
+            'xl': '1.25rem',
+            '2xl': '1.563rem',
+            '3xl': '1.953rem',
+            '4xl': '2.441rem',
+            '5xl': '3.052rem',
+          },
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                whiteSmoke: 'hsl(0, 0%, 96%)',
+                darkBlue: 'hsl(209, 99%, 27%)',
+                lightBlue: 'hsl(218, 73%, 53%)',
+                lightBlack: 'hsl(0, 1%, 30%)',
+              }
         },
     },
 
