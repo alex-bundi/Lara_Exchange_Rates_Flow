a895b00df4ede42c2ecb17ccf569ae02ef7cabdd
Alex Bundi <bundialex45@gmail.com>
2024-02-15 16:02:25 +0300

    User Processes

--- app/Http/Controllers/CurrencyCodeController.php
+++ app/Http/Controllers/CurrencyCodeController.php
@@ -3,20 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CurrencyCodeController extends Controller
 {
     public function getCurrencyCodes() {
         //url('storage/logo/exchange_rate.svg')
-        try {
-            $currencyCodesData = fopen(url('storage/logo/exchange_rate.svg'), 'r');
-            echo('$currencyCodesData');
-        }
-        catch (Exception $e) {
-            if ($e instanceof FatalError) {
-                echo('error');
+        // try {
+        //     $currencyCodesData = fopen(url('storage/logo/exchange_rate.svg'), 'r');
+        //     echo('$currencyCodesData');
+        // }
+        // catch (Exception $e) {
+        //     return response()->json(['message' => 'unsuccessful']);
+        // }
+        $responseType = [
+            'pathNotFound' => 'Path not found',
+            'fileNotFound' => 'Cannot open the file',
+            'success' => 'success'
+        ];
+
+        $url = Storage::path('public\csv\Currency Codes.csv'); // Gets the path to the csv file for currency codes.
+        
+        if (!Storage::exists($url)) {
+            return response()->json(['message' => $responseType['pathNotFound']]);
+        } else {
+            $currencyCodesData = fopen($url, 'r');
+            if ($currencyCodesData === false) {
+                return response()->json(['message' => $responseType['fileNotFound']]);
+            } else {
+                return response()->json(['message' => $responseType['success']]);
+
             }
         }
+
+
+        // $currencyCodesData = fopen($url, 'r');
+        // $responseType = [
+        //     'fileNotFound' => 'Cannot open the file'
+        // ];
+
+        // if ($currencyCodesData === false) {
+        //     return response()->json(['message' => $responseType['fileNotFound']]);
+        // } else {
+        //     while (($row = fgetcsv($currencyCodesData)) !== false) {
+        //         $data[] = $row;
+        //     }
+        //     fclose($currencyCodesData);
+        // }
+       
     }
 
     public function test() {
--- resources/js/index.js
+++ resources/js/index.js
@@ -7,3 +7,13 @@ function toggleLoader (){
     let loader = document.getElementById("preloader");
     loader.style.display ="none";
 }
+
+function getCurrencyCodes () {
+    url = 'http://127.0.0.1:8000/currencyCodeData';
+    fetch(url)
+    .then(function(res){
+        console.log(res.text())
+    })
+}
+
+getCurrencyCodes();
