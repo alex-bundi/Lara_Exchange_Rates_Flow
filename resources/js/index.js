ed66693828f48ddf560c3b107fb02d5de689f97b
Alex Bundi <bundialex45@gmail.com>
2024-02-20 09:08:25 +0300

    currency codes

--- resources/js/index.js
+++ resources/js/index.js
@@ -9,12 +9,32 @@ function toggleLoader (){
 }
 
 async function getCurrencyCodes () {
+    let fromInput = document.getElementById('from');
+    let toInput = document.getElementById('to');
+
+    
     try {
         await fetch('http://127.0.0.1:8000/currencyCodeData')
         .then((res) => res.json())
         .then((res) => {
-            console.log(JSON.stringify(res[0][0])) 
+            res.forEach((item) => {
+
+                item.forEach((val) => {
+                    // console.log(val)
+                    // From dropdown options
+                    let fromOptions = document.createElement('option');
+                    fromOptions.text = `${val[0]} - ${val[2]}`;
+                    fromInput.add(fromOptions)
+
+                    // To dropdown options
+                    let toOptions = document.createElement('option');
+                    toOptions.text = `${val[0]} - ${val[2]}`;
+                    toInput.add(toOptions)
+                }  
+                );
+            });
         })
+       
     } catch (error) {
         console.error('Error fetching data:', error);
     }
