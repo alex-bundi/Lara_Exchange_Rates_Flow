7eb3e34695b270a0b230210ab0ca3618259fa624
Alex Bundi <bundialex45@gmail.com>
2024-02-23 16:23:26 +0300

    currency codes

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,77 +1,77 @@
-window.addEventListener("load", function(){
-    toggleLoader();
+// window.addEventListener("load", function(){
+//     toggleLoader();
     
-})
+// })
 
-function toggleLoader (){
-    let loader = document.getElementById("preloader");
-    loader.style.display ="none";
-}
+// function toggleLoader (){
+//     let loader = document.getElementById("preloader");
+//     loader.style.display ="none";
+// }
 
-async function getCurrencyCodes () {
-    let fromInput = document.getElementById('from');
-    let toInput = document.getElementById('to');
+// async function getCurrencyCodes () {
+//     let fromInput = document.getElementById('from');
+//     let toInput = document.getElementById('to');
 
     
-    try {
-        await fetch('http://127.0.0.1:8000/currencyCodeData')
-        .then((res) => res.json())
-        .then((res) => {
-            res.forEach((item) => {
+//     try {
+//         await fetch('http://127.0.0.1:8000/currencyCodeData')
+//         .then((res) => res.json())
+//         .then((res) => {
+//             res.forEach((item) => {
 
-                item.forEach((val) => {
-                    // console.log(val)
-                    // From dropdown options
-                    let fromOptions = document.createElement('option');
-                    fromOptions.text = `${val[0]} - ${val[2]}`;
-                    fromInput.add(fromOptions)
+//                 item.forEach((val) => {
+//                     // console.log(val)
+//                     // From dropdown options
+//                     let fromOptions = document.createElement('option');
+//                     fromOptions.text = `${val[0]} - ${val[2]}`;
+//                     fromInput.add(fromOptions)
 
-                    // To dropdown options
-                    let toOptions = document.createElement('option');
-                    toOptions.text = `${val[0]} - ${val[2]}`;
-                    toInput.add(toOptions);
-                }  
-                );
-            });
-        })
+//                     // To dropdown options
+//                     let toOptions = document.createElement('option');
+//                     toOptions.text = `${val[0]} - ${val[2]}`;
+//                     toInput.add(toOptions);
+//                 }  
+//                 );
+//             });
+//         })
        
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//     }
 
-}
+// }
 
-getCurrencyCodes();
+// getCurrencyCodes();
 
-// To display input error messages
-function error (message) {
+// // To display input error messages
+// function error (message) {
     
-    message = message;
-    const errorMessageElement = document.getElementById('error_message');
+//     message = message;
+//     const errorMessageElement = document.getElementById('error_message');
 
-    errorMessageElement.textContent = message;
-    errorMessageElement.style.visibility = "visible";
-    errorMessageElement.style.color = "red";
+//     errorMessageElement.textContent = message;
+//     errorMessageElement.style.visibility = "visible";
+//     errorMessageElement.style.color = "red";
 
-    setTimeout(() => errorMessageElement.remove(), 3000); // Remove warning after 3 secs
-}
+//     setTimeout(() => errorMessageElement.remove(), 3000); // Remove warning after 3 secs
+// }
 
-function getCurrentRates () {
+// function getCurrentRates () {
 
-    let ratesForm = document.getElementById('conversion_rates');
-    ratesForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+//     let ratesForm = document.getElementById('conversion_rates');
+//     ratesForm.addEventListener('submit', (event) => {
+//         event.preventDefault();
         
-        let amountInput = Number(document.getElementById('currency_amount').value.trim());
-        let fromInput = document.getElementById('from');
-        let toInput = document.getElementById('to');
-        console.log(amountInput);
-        if (!amountInput) {
-            error('Please fill in the field to search for value...');
-        }
-    })
-}
-getCurrentRates();
+//         let amountInput = Number(document.getElementById('currency_amount').value.trim());
+//         let fromInput = document.getElementById('from');
+//         let toInput = document.getElementById('to');
+//         console.log(amountInput);
+//         if (!amountInput) {
+//             error('Please fill in the field to search for value...');
+//         }
+//     })
+// }
+// getCurrentRates();
 
 
 
